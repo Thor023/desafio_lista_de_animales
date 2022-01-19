9e8ad34a07aedf351657e27daca555332f25a82e
PrgoramaThor <felipe.castaneda84@gmail.com>
2022-01-19 11:17:37 -0300

    corregido el ingreso de datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,13 @@
 class Propietario{
     constructor(nombre, direccion, telefono){
-        this._nombre = nombre;
-        this._direccion = direccion;
-        this._telefono = telefono;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
     }
     datosPropietario(){
     let resultado = document.getElementById('resultado');
     resultado.innerHTML =  `<ul> <li> <span>El nombre del dueño es: ${this.nombre} . El domicio es: ${this.direccion}, y el número telefonico de contacto: ${this.telefono}</span> </li> <li ><span>El tipo de animal es: ${this.tipo}, mientras que el nombre de la mascota es:${this.nombreMascota} y la enfermedad es: ${this.enfermedad}</span></li></ul>`
     }
-
-    //getters que no use 
-
-    // get nombre(){
-    //     return this._nombre;
-    // }
-    // set nombre(nuevoNombre){
-    //     this._nombre = nuevoNombre;
-    // }
-    // get direccion(){
-    //     return this._direccion;
-    // }
-    // set direccion(nuevaDireccion){
-    //     this._direccion = nuevaDireccion;
-    // }
-    // get telefono(){
-    //     return this._telefono;
-    // }
-    // set telefono(nuevoTelefono){
-    //     this._telefono = nuevoTelefono;
-    // }
 }
 class Animal extends Propietario{
     constructor(nombre, direccion, telefono,tipo){
@@ -81,20 +60,6 @@ let reciboDatos = (event)=>{
     event.preventDefault();
     document.getElementById('resultado').innerHTML="";
     const mascotaData = registro();
-    console.log(mascotaData);
-// Imprimir en pantalla (no funciona!!!!!!!)
-    // let resultado = document.getElementById('resultado');
-    // resultado.innerHTML =  `<ul> <li> <span>El nombre del dueño es:</span></li></ul>`
-    
-
-    // const nombre = mascotaData.getNombre;
-    // const telefono = mascotaData.getTelefono;
-    // const resultados = document.getElementById('resultado');
-    // const lista = document.createElement('ul');
-    // lista.innerHTML = `Nombre: ${this.nombre} telefono: ${this.telefono} dirección: ${this.direccion}`;
-    // data.appendChild(lista);
-    // document.getElementById("propietario").value = "";
-    // document.getElementById("telefono").value = "";
-    // document.getElementById("direccion").value = "";
+    mascotaData.datosPropietario();
 } 
 formulario.addEventListener("submit",reciboDatos);
